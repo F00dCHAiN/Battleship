b695da192d115dda51aa29494698a443c2cc09d3
F00dCHAiN <saminyaserwork@gmail.com>
2020-03-30 15:44:18 +0600

    better var naming

--- src/battleship/text/TextMessages.java
+++ src/battleship/text/TextMessages.java
@@ -2,47 +2,47 @@ package battleship.text;
 
 public final class TextMessages {
 
-    private static final String howToPlayText = "<html>Guess the position of the enemy battlehship<br/> and hit it three times "
+    private static final String HOW_TO_PLAY = "<html>Guess the position of the enemy battlehship<br/> and hit it three times "
             + "to sink it.<br/>"
             + " But beware! You have only three shots!<br/>Miss three times and the game is over.</html>";
 
-    private static final String winMessage = "<html>You have sunk the enemy ship!<br>"
+    private static final String WIN_MESSAGE = "<html>You have sunk the enemy ship!<br>"
             + "Click \"Restart\" to play again.</html>";
 
-    private static final String loseMessage = "<html>You have failed to sink the enemy ship!<br/>"
+    private static final String LOSE_MESSAGE = "<html>You have failed to sink the enemy ship!<br/>"
             + "Click \"Restart\" to play again.</html>";
 
-    private static final String statsSaved = "You stats have been saved";
+    private static final String STATS_SAVED = "You stats have been saved";
     
-    private static final String credits = "<html>Made by: Yaser, Samin<br/>"
+    private static final String CREDITS = "<html>Made by: Yaser, Samin<br/>"
             + "Under the Supervision of<br/>"
             + "SAZZAD HOSSAIN<br/>"
             + "Assistant Professor, AIUB<br/>";
     
-    private static final String gitLink = "https://github.com/F00dCHAiN/Battleship.git";
+    private static final String GITHUB_LINK = "https://github.com/F00dCHAiN/Battleship.git";
 
     public static String getHowToPlayText() {
-        return howToPlayText;
+        return HOW_TO_PLAY;
     }
 
     public static String getWinMessage() {
-        return winMessage;
+        return WIN_MESSAGE;
     }
 
     public static String getLoseMessage() {
-        return loseMessage;
+        return LOSE_MESSAGE;
     }
 
     public static String getStatsSaved() {
-        return statsSaved;
+        return STATS_SAVED;
     }
     
     public static String getCredits() {
-        return credits;
+        return CREDITS;
     }
     
     public static String getGitLink() {
-        return gitLink;
+        return GITHUB_LINK;
     }
 }
 
